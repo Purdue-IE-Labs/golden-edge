b1006ec86534c4bcaf51188dd14ebec36748014c
Grayson Nocera <gnocera@purdue.edu>
2025-01-27 17:29:50 -0500

    chore: random improvements in edge class

--- src/gedge/__init__.py
+++ src/gedge/__init__.py
@@ -1,1 +1,9 @@
-from .edge.edge import EdgeNodeConfig, EdgeNodeSession, connect
+from .edge.edge import EdgeNodeConfig, EdgeNodeSession
+from .app.app import AppConfig, AppSession 
+from contextlib import contextmanager
+from typing import Generator
+
+@contextmanager
+def connect(config: AppConfig | EdgeNodeConfig) -> Generator[AppSession | EdgeNodeSession, None, None]:
+    with config.connect() as session:
+        yield session
