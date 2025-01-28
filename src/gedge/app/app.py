8698f8c47213f06d682f190542b3969fccbd8fb0
Grayson Nocera <gnocera@purdue.edu>
2025-01-28 15:38:08 -0500

    feat: build out liveliness and pulling meta messages from influxdb

--- src/gedge/app/app.py
+++ src/gedge/app/app.py
@@ -1,7 +1,7 @@
 from contextlib import contextmanager
 from gedge.comm import Comm
 from gedge.proto import State, Meta, TagData
-from typing import Callable, List, Tuple, TypeAlias, TypeVar
+from typing import Callable, List, Tuple, TypeAlias, TypeVar, Any
 import zenoh
 
 class AppConfig:
@@ -18,30 +18,31 @@ class AppConfig:
 StateCallback: TypeAlias = Callable[[State], None]
 MetaCallback: TypeAlias = Callable[[Meta], None]
 TagDataCallback: TypeAlias = Callable[[TagData], None]
+LivelinessCallback: TypeAlias = Callable[[bool, str], None]
 Callbacks: TypeAlias = Tuple[StateCallback, MetaCallback, TagDataCallback]
 class AppSession:
     def __init__(self, config: AppConfig, comm: Comm):
         self._comm = comm
         self.config = config
+        self.nodes: dict[str, List[zenoh.Subscriber]] = {}
 
-    def print_nodes_on_network(self):
-        pass
+    def print_nodes_on_network(self, print_meta: bool = False):
+        print("Nodes on network")
+        messages = self.pull_meta_messages()
+        for name, meta in messages.items():
+            is_online = self._comm.is_online(name)
+            print(f"\t{name}, {"online" if is_online else "offline"}")
+            if print_meta:
+                print(f"\t{meta}")
 
-    def pull_meta_messages(self):
-        # if node not in self.nodes:
-        #     raise ValueError("invalid node")
-        res = self._comm.session.get(self.config.key_prefix + f"/NODE/**/META")
-        # print(f"meta messages: {len(res)}")
-        for r in res:
-            r: zenoh.Reply
-            b = r.result.payload.to_bytes().replace(b"\r", b"")
-            node_name = self._comm.node_name_from_key_expr(str(r.result.key_expr))
-            try:
-                meta = Meta()
-                meta.ParseFromString(b)
-                print(node_name, meta)
-            except Exception as e:
-                print(f"couldn't decode {e}")
+    def print_nodes_connected_to_app(self, print_meta: bool = False):
+        print("Nodes connected to application")
+        for name in self.nodes:
+            print(name)
+
+    def pull_meta_messages(self) -> dict[str, Meta]:
+        messages = self._comm.pull_meta_messages()
+        return messages
     
     def _on_state_default(self):
         pass
@@ -52,34 +53,65 @@ class AppSession:
     def _on_tag_data_default(self):
         pass
 
-    def connect_to_node(self, name: str, on_state: StateCallback, on_meta: MetaCallback, tag_data_callbacks: dict[str, TagDataCallback]):
+    def _on_liveliness_default(self):
+        pass
+
+    def connect_to_node(self, name: str, on_state: StateCallback, on_meta: MetaCallback, on_liveliness_change: LivelinessCallback, tag_data_callbacks: dict[str, TagDataCallback]):
+        print(f"connecting to node {name}")
         self._comm.name = name
+        handlers = []
         def _on_state(sample: zenoh.Sample):
             payload = sample.payload.to_bytes().replace(b"\r", b"")
             state = State()
             state.ParseFromString(payload)
             on_state(state)
+            if not state.online:
+                self.disconnect_from_node(name)
 
         def _on_meta(sample: zenoh.Sample):
             payload = sample.payload.to_bytes().replace(b"\r", b"")
             meta = Meta()
             meta.ParseFromString(payload)
             on_meta(meta)
+
+        def _on_liveliness(sample: zenoh.Sample):
+            if sample.kind == zenoh.SampleKind.PUT:
+                print(f"node {sample.key_expr} online")
+            elif sample.kind == zenoh.SampleKind.DELETE:
+                print(f"node {sample.key_expr} went offline")
+            is_online = sample.kind == zenoh.SampleKind.PUT
+            on_liveliness_change(is_online, name)
+            if not is_online:
+                self.disconnect_from_node(name)
         
         for tag_key_prefix in tag_data_callbacks:
-            self.add_tag_data_callback(name, tag_key_prefix, tag_data_callbacks[tag_key_prefix])
-
-        # self._comm.session.declare_subscriber(self.config.key_prefix + f"/NODE/{name}/STATE", _on_state)
-        # self._comm.session.declare_subscriber(self.config.key_prefix + f"/NODE/{name}/META", _on_meta)
-        self._comm.session.declare_subscriber(self._comm._state_key_prefix, _on_state)
-        self._comm.session.declare_subscriber(self._comm._meta_key_prefix, _on_meta)
+            subscriber = self.add_tag_data_callback(name, tag_key_prefix, tag_data_callbacks[tag_key_prefix])
+            handlers.append(subscriber)
+        self._comm.name = name
+        subscriber = self._comm.session.declare_subscriber(self._comm._state_key_prefix, _on_state)
+        handlers.append(subscriber)
+        subscriber = self._comm.session.declare_subscriber(self._comm._meta_key_prefix, _on_meta)
+        handlers.append(subscriber)
+        subscriber = self._comm.declare_liveliness_subscriber(_on_liveliness)
+        handlers.append(subscriber)
+        self.nodes[name] = handlers
         self._comm.name = self.config.name
+
+    def disconnect_from_node(self, name: str):
+        if name not in self.nodes:
+            raise IndexError(f"{name} not connected")
+        handlers = self.nodes[name]
+        for h in handlers:
+            h.undeclare()
+        del self.nodes[name]
     
-    def add_tag_data_callback(self, name: str, tag_key_prefix: str, on_tag_data: TagDataCallback):
-        # key_expr = self.config.key_prefix + f"/NODE/{name}/TAGS/DATA/{tag_key_prefix}"
+    def add_tag_data_callback(self, name: str, tag_key_prefix: str, on_tag_data: TagDataCallback) -> zenoh.Subscriber:
+        self._comm.name = name
         def _on_tag_data(sample: zenoh.Sample):
             payload = sample.payload.to_bytes().replace(b"\r", b"")
             tag_data = TagData()
             tag_data.ParseFromString(payload)
             on_tag_data(tag_data)
-        self._comm.session.declare_subscriber(self._comm._tag_data_key_prefix + f"/{tag_key_prefix}", _on_tag_data)
+        subscriber = self._comm.session.declare_subscriber(self._comm._tag_data_key_prefix + f"/{tag_key_prefix}", _on_tag_data)
+        self._comm.name = self.config.name
+        return subscriber
--- src/gedge/comm/comm.py
+++ src/gedge/comm/comm.py
@@ -2,6 +2,7 @@ import zenoh
 from contextlib import contextmanager
 from gedge.edge.tag import Tag
 from gedge.proto import Meta, TagData, DataType, State
+from typing import Callable
 
 # handle Zenoh communications
 # The user will not interact with this item, so we can assume in all functions that zenoh is connected
@@ -21,7 +22,7 @@ class Comm:
     @name.setter
     def name(self, value: str):
         self._name = value
-        self._set_keys(self.key_prefix)
+        self._set_keys()
 
     @property
     def key_prefix(self):
@@ -31,22 +32,49 @@ class Comm:
     def key_prefix(self, value: str):
         value = value.strip("/")
         self._key_prefix = value
-        self._set_keys(value)
+        self._set_keys()
 
-    def _set_keys(self, prefix: str):
-        self._node_key_prefix = prefix + f"/NODE/{self.name}"
-        self._meta_key_prefix = self._node_key_prefix + "/META"
-        self._tag_key_prefix = self._node_key_prefix + "/TAGS"
-        self._tag_data_key_prefix = self._tag_key_prefix + "/DATA"
-        self._tag_write_key_prefix = self._tag_key_prefix + "/WRITES"
-        self._state_key_prefix = self._node_key_prefix + "/STATE"
+    def _set_keys(self):
+        self._node_key_prefix = self.node_key_prefix(self.key_prefix, self.name)
+        self._meta_key_prefix = self.meta_key_prefix(self.key_prefix, self.name)
+        self._tag_data_key_prefix = self.tag_data_key_prefix(self.key_prefix, self.name)
+        self._tag_write_key_prefix = self.tag_write_key_prefix(self.key_prefix, self.name)
+        self._state_key_prefix = self.state_key_prefix(self.key_prefix, self.name)
+
+    def node_key_prefix(self, prefix: str, name: str):
+        node_key_prefix = prefix + f"/NODE/{name}"
+        return node_key_prefix
+
+    def meta_key_prefix(self, prefix: str, name: str):
+        meta_key_prefix = prefix + f"/NODE/{name}/META"
+        return meta_key_prefix
+
+    def tag_data_key_prefix(self, prefix: str, name: str):
+        return self.node_key_prefix(prefix, name) + "/TAGS/DATA"
     
-    def node_name_from_key_expr(self, key_expr: str):
-        components = key_expr.split("/")
+    def tag_write_key_prefix(self, prefix: str, name: str):
+        return self.node_key_prefix(prefix, name) + "/TAGS/WRITE"
+    
+    def state_key_prefix(self, prefix: str, name: str):
+        return self.node_key_prefix(prefix, name) + "/STATE"
+
+    def liveliness_key_prefix(self, prefix: str, name: str):
+        return self.node_key_prefix(prefix, name)
+    
+    def node_name_from_key_expr(self, key_expr: str | zenoh.KeyExpr):
+        components = str(key_expr).split("/")
         return components[components.index("NODE") + 1]
     
     def declare_liveliness_token(self) -> zenoh.LivelinessToken:
+        print(f"declaring liveliness token on {self._node_key_prefix}")
         return self.session.liveliness().declare_token(self._node_key_prefix)
+
+    def declare_liveliness_subscriber(self, handler: Callable[[zenoh.Sample], None]) -> zenoh.Subscriber:
+        print(f"declaring subscriber of liveliness of {self._node_key_prefix}")
+        return self.session.liveliness().declare_subscriber(self._node_key_prefix, handler)
+
+    def query_liveliness(self, key_expr: str) -> zenoh.Reply:
+        return self.session.liveliness().get(key_expr).recv()
 
     @contextmanager
     def connect(self):
@@ -69,5 +97,31 @@ class Comm:
         print(f"sending state on key expression: {self._state_key_prefix}")
         res = self.session.put(self._state_key_prefix, state.SerializeToString())
 
-    def pull_meta_messages(self) -> list[Meta]:
-        res = self.session.get("**/META/**")
+    def pull_meta_messages(self, only_online: bool = False) -> dict[str, Meta]:
+        res = self.session.get(f"{self.key_prefix}/NODE/*/META")
+        messages = {}
+        for r in res:
+            r: zenoh.Reply
+            if not r.ok:
+                continue
+            result = r.result
+
+            b = result.payload.to_bytes().replace(b"\r", b"")
+            node_name = self.node_name_from_key_expr(result.key_expr)
+            try:
+                meta = Meta()
+                meta.ParseFromString(b)
+                messages[node_name] = meta
+            except Exception as e:
+                print(f"couldn't decode {e}")
+        return messages
+            
+    def is_online(self, name: str) -> bool:
+        # this command will fail is no token is declared for this prefix, meaning offline
+        try:
+            reply = self.session.liveliness().get(self.node_key_prefix(self.key_prefix, name)).recv()
+            if not reply.ok:
+                return False
+            return reply.result.kind == zenoh.SampleKind.PUT
+        except:
+            return False
