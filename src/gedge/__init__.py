191069b9285d3b3da1f6529a9e18d8b1244f72b2
Grayson Nocera <gnocera@purdue.edu>
2025-02-03 15:18:18 -0500

    refactor: utilize overload typing for typehints in client scripts

--- src/gedge/__init__.py
+++ src/gedge/__init__.py
@@ -1,5 +1,15 @@
 from .edge.edge import EdgeNodeConfig, EdgeNodeSession
 from .app.app import AppConfig, AppSession 
 
-def connect(config: AppConfig | EdgeNodeConfig) -> AppSession | EdgeNodeSession:
+from typing import overload
+
+@overload
+def connect(config: EdgeNodeConfig) -> EdgeNodeSession:
+    return config.connect()
+
+@overload
+def connect(config: AppConfig) -> AppSession:
+    return config.connect()
+
+def connect(config):
     return config.connect()
