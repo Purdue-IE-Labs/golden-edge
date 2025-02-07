699d1e59ed6205b8e3ef57d05b5e86df82c13dd4
Grayson Nocera <gnocera@purdue.edu>
2025-02-07 14:57:37 -0500

    feat: combine node and app

--- src/gedge/__init__.py
+++ src/gedge/__init__.py
@@ -1,5 +1,7 @@
 from .edge.edge import EdgeNodeConfig, EdgeNodeSession
 from .app.app import AppConfig, AppSession 
+from .node.node import NodeConfig, NodeSession
+from .node.remote import RemoteConfig, RemoteConnection
 
 from typing import overload
 
@@ -9,6 +11,10 @@ def connect(config: EdgeNodeConfig) -> EdgeNodeSession:
 
 @overload
 def connect(config: AppConfig) -> AppSession:
+    return config.connect()
+
+@overload
+def connect(config: NodeConfig) -> NodeSession:
     return config.connect()
 
 def connect(config):
