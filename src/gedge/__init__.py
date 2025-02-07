3a8f7205b443792af2e303d989975aa9099bbade
Grayson Nocera <gnocera@purdue.edu>
2025-02-07 15:13:46 -0500

    chore: remove old connect functions

--- src/gedge/__init__.py
+++ src/gedge/__init__.py
@@ -1,21 +1,4 @@
-from .edge.edge import EdgeNodeConfig, EdgeNodeSession
-from .app.app import AppConfig, AppSession 
 from .node.node import NodeConfig, NodeSession
-from .node.remote import RemoteConfig, RemoteConnection
 
-from typing import overload
-
-@overload
-def connect(config: EdgeNodeConfig) -> EdgeNodeSession:
-    return config.connect()
-
-@overload
-def connect(config: AppConfig) -> AppSession:
-    return config.connect()
-
-@overload
 def connect(config: NodeConfig) -> NodeSession:
-    return config.connect()
-
-def connect(config):
     return config.connect()
