8843f8c436f62402a888838442217b7b2f83358a
Grayson Nocera <gnocera@purdue.edu>
2025-02-25 14:52:48 -0500

    chore: add todo for future work

--- src/gedge/node/codes.py
+++ src/gedge/node/codes.py
@@ -2,6 +2,8 @@
 
 # temporary, this will change
 # TODO: add decorators to check conditions of input arguments
+# reserved: 0-99 (TODO: check in outward facing functions that they don't violate this)
+# TODO: change classes to dataclasses
 
 DONE = 10
 METHOD_ERROR = 20
