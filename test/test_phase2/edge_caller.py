634d50502c22b2756dd18ab3f6d810484e2a31d3
Grayson Nocera <gnocera@purdue.edu>
2025-03-05 17:43:40 -0500

    docs: add some logging for timeout and an example of calling a method

--- test/test_phase2/edge_caller.py
+++ test/test_phase2/edge_caller.py
@@ -6,7 +6,21 @@ def on_reply(reply: gedge.Reply):
     code, body, error = reply.code, reply.body, reply.error
     print(f"received code {code}")
 
-with gedge.connect(gedge.NodeConfig("BuildAtScale/Robots/Methods/Demo/Caller")) as session:
+with gedge.connect(gedge.NodeConfig("BuildAtScale/Robots/Methods/Demo/Caller"), "tcp/localhost:7447") as session:
     remote = session.connect_to_remote("BuildAtScale/Robots/Methods/Demo/Callee")
-    remote.call_method("start/project", on_reply, name="testing2", speed=90)
+    print("first method call")
+    responses1 = remote.call_method_iter("start/project", name="testing2", speed=90)
+    for response in responses1:
+        print(response)
+        if response.code == 202:
+            break
+    print("second method call")
+    responses2 = remote.call_method_iter("start/project", name="testing", speed=80)
+    for response in responses2:
+        print(response)
+    
+    # we can collect responses that we didn't see earlier because we said "break" before iterating through all responses
+    for response in responses1:
+        print(response)
+
     time.sleep(60)
