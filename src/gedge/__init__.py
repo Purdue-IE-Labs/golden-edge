de9a018887d3be0aa6e490a318d0663e2bb27c41
Grayson Nocera <gnocera@purdue.edu>
2025-03-05 17:27:17 -0500

    refactor: add option to add IPs (in the future, we would want to change this to be a key expression, for example)

--- src/gedge/__init__.py
+++ src/gedge/__init__.py
@@ -10,5 +10,8 @@ import os
 level = os.environ.get("LOG_LEVEL", "INFO").upper()
 logging.basicConfig(level=level)
 
-def connect(config: NodeConfig) -> NodeSession:
-    return config.connect()
+def connect(config: NodeConfig, *connections: str) -> NodeSession:
+    conns = list(connections)
+    if len(conns) == 0:
+        raise ValueError("Must provide at least one connection point")
+    return config._connect(conns)
