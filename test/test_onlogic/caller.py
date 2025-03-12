2032c3af75e479763074812af4342fafb2df9428
Grayson Nocera <gnocera@purdue.edu>
2025-03-12 14:30:44 -0400

    test: refactor caller node to test vice integration

--- test/test_onlogic/caller.py
+++ test/test_onlogic/caller.py
@@ -1,16 +1,25 @@
 import gedge
 import time
 
+# quick demo to open, then close the vice
+
+def open_vice():
+    print("opening vice")
+    time.sleep(2)
+    reply = remote.write_tag("vice/open", True)
+    print(reply.code)
+    time.sleep(2)
+
+def close_vice():
+    print("closing vice")
+    time.sleep(2)
+    reply = remote.write_tag("vice/open", False)
+    print(reply.code)
+    time.sleep(2)
+
 config = gedge.NodeConfig("BuildScale/Lathe/Caller")
 with gedge.connect(config, "tcp/192.168.4.60:7447") as session:
     remote = session.connect_to_remote("BuildScale/Lathe/Onlogic")
-    time.sleep(1)
-    print()
-    print("opening vice")
-    reply = remote.write_tag("vice/open", True)
-    print(reply.code)
-    print()
     time.sleep(5)
-    print("closing vice")
-    reply = remote.write_tag("vice/open", False)
-    print(reply.code)
+    open_vice()
+    close_vice()
