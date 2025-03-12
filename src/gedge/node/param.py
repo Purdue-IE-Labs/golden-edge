c65639736f72e397bcaea33721fe0de8bf1b0bab
Grayson Nocera <gnocera@purdue.edu>
2025-03-12 17:12:49 -0400

    refactor: fix circular imports and create BodyData and ParamData objects for the user to use

--- src/gedge/node/param.py
+++ src/gedge/node/param.py
@@ -3,6 +3,7 @@ from multiprocessing import Value
 from typing import Any, Self
 
 from gedge.node.data_type import DataType
+from gedge.node.gtypes import TagValue
 from gedge.node.prop import Props
 from gedge import proto
 
@@ -32,3 +33,8 @@ class Param:
         type = DataType.from_json5(param["type"])
         props = Props.from_json5(param.get("props", {}))
         return cls(type, props)
+
+@dataclass
+class ParamData:
+    value: TagValue
+    props: dict[str, TagValue]
