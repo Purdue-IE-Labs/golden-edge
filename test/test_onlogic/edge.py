13fcbbb12878f8f02fe87f80f3b0a56aed554046
Grayson Nocera <gnocera@purdue.edu>
2025-03-12 14:11:07 -0400

    test: finish onlogic edge node

--- test/test_onlogic/edge.py
+++ test/test_onlogic/edge.py
@@ -4,54 +4,41 @@ import gedge
 import serial
 import time
 
+def open_vice_command(group: int = 0, pin: int = 0) -> str:
+    command = f"dio set DO_G{group} {pin} false\n"
+    return command
+
+def close_vice_command(group: int = 0, pin: int = 0) -> str:
+    command = f"dio set DO_G{group} {pin} true\n"
+    return command
+
+def read_vice_state_command(group: int = 0, pin: int = 0) -> str:
+    command = f"dio get DO_G{group} output {pin}\n"
+    return command
+
 print()
 dio_port = '/dev/ttyACM0'
 with serial.Serial(dio_port) as mcu:
-    def terminal_command(command: str):
+    def run_command(command: str):
+        time.sleep(0.005)
         print(f"Running command: {command}")
         mcu.write(command.encode())
         time.sleep(0.005)
         res = mcu.read(mcu.in_waiting).decode()
         print(f"res: '{res}'")
-        time.sleep(0.005)
         parts = res.split("\n")
+        print(parts)
         if len(parts) > 1:
             res = parts[1].strip("\r")
         return res
 
-    # time.sleep(0.005)
-    # mcu.write(b'\r\n')
-    # time.sleep(0.005)
-    # mcu.read(mcu.in_waiting)
-    # # command = "dio num D1_G0 outputs\n"
-    # # command = "history\n"
-    # # command  = "device list\n"
-    # # mcu.write(command.encode())
-    # # time.sleep(0.005)
-    # # res = mcu.read(mcu.in_waiting).decode()
-    # # print(res)
-
-    # # get output of DO_G0
-    # command = "dio get DO_G0 output 0\n"
-    # terminal_command(command)
-    # # set DO_G0 to off, 0V
-    # command = "dio set DO_G0 0 false\n"
-    # terminal_command(command)
-
-    # command = "dio get DO_G0 output 0\n"
-    # terminal_command(command)
-
-
     def handler(query: gedge.TagWriteQuery) -> None:
-        # attempt to close or open the vice
         if query.value == True:
-            # open the vice
-            command = "dio set DO_G0 0 false\n"
-            terminal_command(command)
+            command = open_vice_command(0, 0)
+            res = run_command(command)
         elif query.value == False:
-            # close the vice
-            command = "dio set DO_G0 0 true\n"
-            terminal_command(command)
+            command = close_vice_command(0, 0)
+            res = run_command(command)
         query.reply(200)
 
     here = pathlib.Path(__file__).parent
@@ -60,8 +47,8 @@ with serial.Serial(dio_port) as mcu:
 
     with gedge.connect(config, "tcp/192.168.4.60:7447") as session:
         # read initial value of vice
-        command = "dio get DO_G0 output 0\n"
-        state = terminal_command(command).strip('\n\r ')
+        command = read_vice_state_command(0, 0)
+        state = run_command(command)
         initial_value = False if state == '1' else True
         session.update_tag("vice/open", initial_value)
         print(f"initial value of vice is {initial_value}")
