1d32ed7e09a7749d5345ab3b2efd6cf01d82a89b
Grayson Nocera <gnocera@purdue.edu>
2025-03-12 16:49:40 -0400

    test: add props to json5 tester to see if it works

--- test/test_methods/caller.py
+++ test/test_methods/caller.py
@@ -8,12 +8,12 @@ with gedge.connect(config, "tcp/localhost:7447") as session:
     print("FIRST METHOD CALL")
     responses = remote.call_method_iter("call/method", name="super long things that should get rejected by func", speed=100)
     for response in responses:
-        print(response.code, response.props)
+        print(response.code, response.props, response.body)
     print("\n\nSECOND METHOD CALL")
     responses2 = remote.call_method_iter("call/method", name="hello world", speed=200)
     for response in responses2:
-        print(response.code, response.props)
+        print(response.code, response.props, response.body)
     print("\n\nTHIRD METHOD CALL")
     responses3 = remote.call_method_iter("call/method", name="hello world", speed=40)
     for response in responses3:
-        print(response.code, response.props)
+        print(response.code, response.props, response.body.get('res1', {}).get('value', None), response.body.get('res1', {}).get('props', None))
