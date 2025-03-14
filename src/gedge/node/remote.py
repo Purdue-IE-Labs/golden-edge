3657b6c6c4cb1f11e17939455cbd225d6240a6af
Grayson Nocera <gnocera@purdue.edu>
2025-03-14 15:59:49 -0400

    refactor: move logic for defining zenoh queries into comm

--- src/gedge/node/remote.py
+++ src/gedge/node/remote.py
@@ -61,42 +61,7 @@ class RemoteConnection:
     def __exit__(self, *exc):
         self.close()
         self._comm.__exit__(*exc)
-
-    def _on_tag_data(self, on_tag_data: TagDataCallback) -> ZenohCallback:
-        def _on_tag_data(sample: zenoh.Sample):
-            tag_data: proto.TagData = self._comm.deserialize(proto.TagData(), sample.payload.to_bytes())
-            path: str = NodeKeySpace.tag_path_from_key(str(sample.key_expr))
-            if path not in self.tags:
-                node: str = NodeKeySpace.name_from_key(str(sample.key_expr))
-                raise TagLookupError(path, node)
-            tag_config = self.tags[path]
-            value = TagData.proto_to_py(tag_data, tag_config.type)
-            logger.info(f"Remote node {self.key} updating tag {path} with value {value}")
-            on_tag_data(str(sample.key_expr), value)
-        return _on_tag_data
-
-    def _on_state(self, on_state: StateCallback) -> ZenohCallback:
-        def _on_state(sample: zenoh.Sample):
-            state: proto.State = self._comm.deserialize(proto.State(), sample.payload.to_bytes())
-            logger.info(f"Remote node {self.key} publishing state message")
-            on_state(str(sample.key_expr), state)
-        return _on_state
-
-    def _on_meta(self, on_meta: MetaCallback) -> ZenohCallback:
-        def _on_meta(sample: zenoh.Sample):
-            meta: proto.Meta = self._comm.deserialize(proto.Meta(), sample.payload.to_bytes())
-            logger.info(f"Remote node {self.key} publishing meta message")
-            on_meta(str(sample.key_expr), meta)
-        return _on_meta
     
-    def _on_liveliness(self, on_liveliness_change: LivelinessCallback) -> ZenohCallback:
-        def _on_liveliness(sample: zenoh.Sample):
-            is_online = sample.kind == zenoh.SampleKind.PUT
-            online = "online" if is_online else "offline"
-            logger.info(f"Liveliness of remote node {self.key} changed: {online}")
-            on_liveliness_change(str(sample.key_expr), is_online)
-        return _on_liveliness
-
     def close(self):
         for sub in self._subscriptions:
             sub.undeclare()        
@@ -108,23 +73,19 @@ class RemoteConnection:
             raise TagLookupError(path, self.ks.name)
 
         self.config.read_write_tags.append(path)
-        _on_tag_data = self._on_tag_data(on_tag_data)
-        subscriber = self._comm.tag_data_subscriber(self.ks, path, _on_tag_data)
+        subscriber = self._comm.tag_data_subscriber(self.ks, path, on_tag_data, self.tags)
         self._subscriptions.append(subscriber)
 
     def add_state_callback(self, on_state: StateCallback) -> None:
-        _on_state = self._on_state(on_state)
-        subscriber = self._comm.state_subscriber(self.ks, _on_state)
+        subscriber = self._comm.state_subscriber(self.ks, on_state)
         self._subscriptions.append(subscriber)
 
     def add_meta_callback(self, on_meta: MetaCallback) -> None:
-        _on_meta = self._on_meta(on_meta)
-        subscriber = self._comm.meta_subscriber(self.ks, _on_meta)
+        subscriber = self._comm.meta_subscriber(self.ks, on_meta)
         self._subscriptions.append(subscriber)
 
     def add_liveliness_callback(self, on_liveliness_change: LivelinessCallback) -> None:
-        _on_liveliness = self._on_liveliness(on_liveliness_change)
-        subscriber = self._comm.liveliness_subscriber(self.ks, _on_liveliness)
+        subscriber = self._comm.liveliness_subscriber(self.ks, on_liveliness_change)
         self._subscriptions.append(subscriber)
 
     def tag_binds(self, paths: list[str]) -> list[TagBind]:
@@ -167,41 +128,6 @@ class RemoteConnection:
     async def write_tag_async(self, path: str, value: Any) -> TagWriteReply:
         return self._write_tag(path, value)
     
-    def _on_reply(self, path: str, on_reply: MethodReplyCallback) -> ZenohCallback:
-        def _on_reply(sample: zenoh.Sample) -> None:
-            if not sample:
-                print("warning: reply super not ok")
-                return
-            r: proto.ResponseData = self._comm.deserialize(proto.ResponseData(), sample.payload.to_bytes())
-
-            '''
-            Design decision here. The problem is that golden-edge reserved codes do not have a 
-            config backing (we could add one if we wanted), so we have to create a config on 
-            the fly to give back to the user for them to look at. For now, it's empty. At config 
-            initialization, we could (and maybe should) inject these. But then if the users 
-            goes len(responses) they may be confused to find that we have added some without 
-            there permission. Or maybe we just never show the user any of these. But also 
-            they need to see the error ones to know that something went terribly wrong.
-            '''
-            if r.code in {codes.DONE, codes.METHOD_ERROR}:
-                response: MethodResponse = MethodResponse(r.code, Props.empty(), {})
-            else:
-                response: MethodResponse = self.responses[path][r.code]
-            body: dict[str, BodyData] = {}
-            for key, value in r.body.items():
-                data_type = response.body[key].type
-                props = response.body[key].props
-                body[key] = BodyData(TagData.proto_to_py(value, data_type), props.to_value())
-            # method_config = self.methods[path] 
-            props = response.props.to_value()
-            reply = MethodReply(str(sample.key_expr), r.code, body, r.error, props)
-            on_reply(reply)
-            if r.code in {codes.DONE, codes.METHOD_ERROR}:
-                # remove subscription after we are done
-                logger.debug(f"remove subscription for key expr {sample.key_expr}")
-                self._subscriptions.remove([x for x in self._subscriptions if x.key_expr == sample.key_expr][0])
-        return _on_reply
-    
     # TODO: (key, value) vs {key: value}. Currently, we use the tuple for (name, type) and the dict for {name: value}
     def call_method(self, path: str, on_reply: MethodReplyCallback, **kwargs) -> None:
         # TODO: this setup may limit the user if they want to have a parameter "path" passed into 
@@ -211,6 +137,7 @@ class RemoteConnection:
             raise MethodLookupError(path, self.ks.name)
         
         # TODO: should this be a queryable with selectors? 
+        # TODO: should this be in comm?
         method_query_id = str(uuid.uuid4())
         method = self.methods[path]
         params: dict[str, proto.TagData] = {}
@@ -218,14 +145,11 @@ class RemoteConnection:
             data_type = method.params[key].type
             params[key] = TagData.py_to_proto(value, data_type)
 
-        on_reply_: ZenohCallback = self._on_reply(path, on_reply)
         # TODO: we need a subscriber for /** and for /caller_id/method_query_id
         # subscriber = self._comm.method_queryable_v2(self.ks, path, caller_id, method_query_id, on_reply)
         # self._subscriptions.append(subscriber)
         logger.info(f"Querying method of node {self.ks.name} at path {path} with params {params.keys()}")
-        key_expr = self.ks.method_response(path, self.node_id, method_query_id)
-        self._subscriptions.append(self._comm._subscriber(key_expr, on_reply_))
-        self._comm.query_method(self.ks, path, self.node_id, method_query_id, params)
+        self._comm.query_method(self.ks, path, self.node_id, method_query_id, params, on_reply, self.responses[path])
     
     def call_method_iter(self, path: str, timeout: int | None = None, **kwargs) -> Iterator[MethodReply]:
         # appparently, Generator[Reply, None, None] == Iterator[Reply]?
@@ -247,9 +171,8 @@ class RemoteConnection:
             replies.put(reply)
 
         logger.info(f"Querying method of node {self.ks.name} at path {path} with params {params.keys()}")
-        key_expr = self.ks.method_response(path, self.node_id, method_query_id)
-        self._subscriptions.append(self._comm._subscriber(key_expr, self._on_reply(path, _on_reply)))
-        self._comm.query_method(self.ks, path, self.node_id, method_query_id, params)
+        # TODO: this function definition is longggggg, so many arguments
+        self._comm.query_method(self.ks, path, self.node_id, method_query_id, params, _on_reply, self.responses[path])
         while True:
             try:
                 res = replies.get(block=True, timeout=timeout)
