3a497c70747961ebab9c435668721eb302293e9b
Grayson Nocera <gnocera@purdue.edu>
2025-03-27 13:10:02 -0400

    refactor: allow users to just pass IP

--- src/gedge/__init__.py
+++ src/gedge/__init__.py
@@ -7,6 +7,7 @@ from .node.method_reply import MethodReply
 from .node.tag_write_query import TagWriteQuery
 from .node.tag_write_reply import TagWriteReply
 from .node.test_node import TestNodeSession
+from .node.subnode import SubnodeSession
 
 import logging
 import os
@@ -14,10 +15,17 @@ import os
 level = os.environ.get("LOG_LEVEL", "INFO").upper()
 logging.basicConfig(level=level)
 
+ZENOH_PORT = 7447
 def connect(config: NodeConfig, *connections: str) -> NodeSession:
     conns = list(connections)
     if len(conns) == 0:
         raise ValueError("Must provide at least one connection point")
+    
+    # allow for the user to pass in just an IP, we complete the rest
+    for i in range(len(conns)):
+        if not (conns[i].startswith("tcp/") and conns[i].endswith(f":{ZENOH_PORT}")):
+            conns[i] = f"tcp/{conns[i]}:{ZENOH_PORT}"
+
     return config._connect(conns)
 
 def mock_connect(config: NodeConfig) -> TestNodeSession:
