9c23590f4f17cf702621db27a2a3397bbd647339
Grayson Nocera <gnocera@purdue.edu>
2025-03-27 14:43:52 -0400

    chore: bump python google package version to match gencode version

--- src/gedge/proto/__init__.py
+++ src/gedge/proto/__init__.py
@@ -7,3 +7,8 @@ from .tag_pb2 import Tag, WriteResponse, WriteResponseData
 from .body_pb2 import Body
 from .param_pb2 import Param
 from .subnode_pb2 import Subnode
+
+"""
+these are generated with proto version 6.30.2,
+so we bind the pyproject.toml to use 6.30.2 as well for google
+"""
