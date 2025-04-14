fac9e42dd24a2713c34c251edf35fa4a6e287b33
Grayson Nocera <gnocera@purdue.edu>
2025-04-14 14:58:02 -0400

    refactor: remove old proto files

--- src/gedge/proto/__init__.py
+++ src/gedge/proto/__init__.py
@@ -1,14 +1,15 @@
-from .meta_pb2 import Meta 
+from .base_data_pb2 import BaseData
+from .body_config_pb2 import BodyConfig
+from .data_model_config_pb2 import DataModelConfig
+from .data_model_session_pb2 import DataModel
+from .data_object_config_pb2 import DataObjectConfig
+from .meta_pb2 import Meta
+from .method_config_pb2 import MethodConfig, MethodResponseConfig
+from .method_session_pb2 import MethodCall, MethodResponse
+from .params_config_pb2 import ParamsConfig
+from .props_pb2 import Props
 from .state_pb2 import State
-from .tag_data_pb2 import TagData, DataType, ListInt, ListBool, ListFloat, ListLong, ListString
-from .method_pb2 import Method, MethodQueryData, Response, ResponseData 
-from .prop_pb2 import Prop
-from .tag_pb2 import Tag, WriteResponse, WriteResponseData
-from .body_pb2 import Body
-from .param_pb2 import Param
-from .subnode_pb2 import Subnode
-
-"""
-these are generated with proto version 6.30.2,
-so we bind the pyproject.toml to use 6.30.2 as well for google
-"""
+from .subnode_config_pb2 import SubnodeConfig
+from .tag_config_pb2 import TagConfig
+from .tag_session_pb2 import TagWriteResponse
+from .type_pb2 import Type, BaseType, DataModelType
