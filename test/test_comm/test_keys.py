52b004d6be00f807420710a7be333d2ca0db9733
Jonas Cowley <jcowley@purdue.edu>
2025-04-22 17:29:16 -0400

    Started test_remote.py and refactored test_keys.py

--- test/test_comm/test_keys.py
+++ test/test_comm/test_keys.py
@@ -3,164 +3,286 @@ import pytest
 
 from gedge.comm import keys
 
+'''
+This test file is organized into five different classes:
+    - TestSanity: 
+        Checks basic expected behaviour from each function
+    - TestWhiteSpace:
+        Checks behaviour of each function when fed whitespace characters 
+            (This is just to ensure that we're following the same behaviour throughout, 
+            we don't want a random function to remove whitespaces)
+    - TestEmpty:
+        Checks how functions respond when fed no parameters
+    - TestUnicode:
+        Checks how functions respond to being fed unicode characters
+    - TestOverlap:
+        In-depth testing of the overlap function (since it has the most edge cases and is the most unique behaviour-wise)
+
+'''
+
+
 class TestSanity:
     def test_key_join(self):
+        # Expected key to be generated
         expected_key = "part0/part1/part2"
+
         assert expected_key == keys.key_join("part0", "part1", "part2")
 
     def test_node_key_prefix(self):
+        # Expected key to be generated
         expected_key = "prefix/NODE/name"
+
         assert expected_key == keys.node_key_prefix(prefix="prefix", name="name")
 
     def test_meta_key_prefix(self):
+        # Expected key to be generated
         expected_key = "prefix/NODE/name/META"
+
         assert expected_key == keys.meta_key_prefix(prefix="prefix", name="name")
 
     def test_tag_data_key_prefix(self):
+        # Expected key to be generated
         expected_key = "prefix/NODE/name/TAGS/DATA"
+
         assert expected_key == keys.tag_data_key_prefix(prefix="prefix", name="name")
 
     def test_tag_data_key(self):
+        # Expected key to be generated
         expected_key = "prefix/NODE/name/TAGS/DATA/key"
 
         assert expected_key == keys.tag_data_key(prefix="prefix", name="name", key="key")
 
     def test_tag_write_key_prefix(self):
+        # Expected key to be generated
         expected_key = "prefix/NODE/name/TAGS/WRITE"
+
         assert expected_key == keys.tag_write_key_prefix(prefix="prefix", name="name")
 
     def test_tag_write_key(self):
+        # Expected key to be generated
         expected_key = "prefix/NODE/name/TAGS/WRITE/key"
+
         assert expected_key == keys.tag_write_key(prefix="prefix", name="name", key="key")
 
     def test_state_key_prefix(self):
+        # Expected key to be generated
         expected_key = "prefix/NODE/name/STATE"
+
         assert expected_key == keys.state_key_prefix(prefix="prefix", name="name")
 
     def test_method_key_prefix(self):
+        # Expected key to be generated
         expected_key = "prefix/NODE/name/METHODS"
+
         assert expected_key == keys.method_key_prefix(prefix="prefix", name="name")
 
     def test_liveliness_key_prefix(self):
+        # Expected key to be generated
         expected_key = "prefix/NODE/name"
+
         assert expected_key == keys.liveliness_key_prefix(prefix="prefix", name="name")
 
     def test_subnodes_key_prefix(self):
+        # Expected key to be generated
         expected_key = "prefix/NODE/node_name/SUBNODES"
+
         assert expected_key == keys.subnodes_key_prefix(prefix="prefix", node_name="node_name")
 
     def test_method_response_from_call(self):
+        # Expected key to be generated
         expected_key = "key_expr/RESPONSE"
+        
         assert expected_key == keys.method_response_from_call(key_expr="key_expr")
 
     def test_internal_to_user_key(self):
+        # Node key generated from node_key_prefix
         node_key = keys.node_key_prefix(prefix="prefix", name="node")
+        
+        # Expected key to be generated
         expected_key = "prefix/node"
+
         assert expected_key == keys.internal_to_user_key(key_expr=node_key)
 
 class TestWhitespace:
-    def test_key_join(self):
-        expected_key = "    p   a   r  t 0/  p  a  r   t      1 /     par t    2"
-        assert expected_key == keys.key_join("    p   a   r  t 0", "  p  a  r   t      1 ", "     par t    2")
-
-    def test_node_key_prefix(self):
-        expected_key = "p    re   f  i x/NODE/n    a    m      e"
-        assert expected_key == keys.node_key_prefix(prefix="p    re   f  i x", name="n    a    m      e")
-
-    def test_meta_key_prefix(self):
-        expected_key = "p    re   f   i x/NODE/   n     a    m  e            /META"
-        assert expected_key == keys.meta_key_prefix(prefix="p    re   f   i x", name="   n     a    m  e            ")
     
-    def test_tag_data_key_prefix(self):
-        expected_key = "p    r   e   f    ix/NODE/   n    a    m e /TAGS/DATA"
-        assert expected_key == keys.tag_data_key_prefix(prefix="p    r   e   f    ix", name="   n    a    m e ")
+    # Parametrized values for whitespace (no key)
+    whitespace_strings_no_key = [
+        ("p    re   f  i x", "     n    a    m      e")
+    ]
 
-    def test_tag_data_key(self):
-        expected_key = "    p   r  e f      i x      /NODE/  n   a  m      e/TAGS/DATA/   k      e     y"
-        assert expected_key == keys.tag_data_key(prefix="    p   r  e f      i x      ", name="  n   a  m      e", key="   k      e     y")
+    # Parametrized values for whitespace (with key)
+    whitespace_strings_key = [
+        ("p    re   f  i x", "n    a    m      e", "   k      e     y")
+    ]
 
-    def test_tag_write_key_prefix(self):
-        expected_key = "    p   r  e    f  ix     /NODE/n     a   m   e/TAGS/WRITE"
-        assert expected_key == keys.tag_write_key_prefix(prefix="    p   r  e    f  ix     ", name="n     a   m   e")
+    @pytest.mark.parametrize("prefix, name", whitespace_strings_no_key, ids=["whitespace test"])
+    def test_key_join(self, prefix, name):
+        # Expected key to be generated
+        expected_key = f"{prefix}/{name}"
 
-    def test_tag_write_key(self):
-        expected_key = "p    r   e  f  i    x/NODE/n    a    m    e/TAGS/WRITE/     k   e  y    "
-        assert expected_key == keys.tag_write_key(prefix="p    r   e  f  i    x", name="n    a    m    e", key="     k   e  y    ")
+        assert expected_key == keys.key_join(prefix, name)
 
-    def test_state_key_prefix(self):
-        expected_key = "pr       e  f  i x   /NODE/   n  a m e     /STATE"
-        assert expected_key == keys.state_key_prefix(prefix="pr       e  f  i x   ", name="   n  a m e     ")
 
-    def test_method_key_prefix(self):
-        expected_key = "     p r  e    f    i    x/NODE/       n     a    m e     /METHODS"
-        assert expected_key == keys.method_key_prefix(prefix="     p r  e    f    i    x", name="       n     a    m e     ")
+    @pytest.mark.parametrize("prefix, name", whitespace_strings_no_key, ids=["whitespace test"])
+    def test_node_key_prefix(self, prefix, name):
+        # Expected key to be generated
+        expected_key = f"{prefix}/NODE/{name}"
 
-    def test_liveliness_key_prefix(self):
-        expected_key = "p     r     e    f    i     x/NODE/      n   a   m e  " 
-        assert expected_key == keys.liveliness_key_prefix(prefix="p     r     e    f    i     x", name="      n   a   m e  ")
+        assert expected_key == keys.node_key_prefix(prefix, name)
 
-    def test_subnodes_key_prefix(self):
-        expected_key = "    p  r e        f   i   x      /NODE/    n  o d e _       n  a m e  /SUBNODES"
-        assert expected_key == keys.subnodes_key_prefix(prefix="    p  r e        f   i   x      ", node_name="    n  o d e _       n  a m e  ")
+    @pytest.mark.parametrize("prefix, name", whitespace_strings_no_key, ids=["whitespace test"])
+    def test_meta_key_prefix(self, prefix, name):
+        # Expected key to be generated
+        expected_key = f"{prefix}/NODE/{name}/META"
 
-    def test_method_response_from_call(self):
-        expected_key = "     k    e  y      _    e  x p r   /RESPONSE"
-        assert expected_key == keys.method_response_from_call(key_expr="     k    e  y      _    e  x p r   ")
+        assert expected_key == keys.meta_key_prefix(prefix, name)
+    
+    @pytest.mark.parametrize("prefix, name", whitespace_strings_no_key, ids=["whitespace test"])
+    def test_tag_data_key_prefix(self, prefix, name):
+        # Expected key to be generated
+        expected_key = f"{prefix}/NODE/{name}/TAGS/DATA"
 
-    def test_internal_to_user_key(self):
-        node_key = keys.node_key_prefix(prefix="     p    r    e   f   i  x     ", name="     n    o d    e     ")
-        expected_key = "     p    r    e   f   i  x     /     n    o d    e     "
+        assert expected_key == keys.tag_data_key_prefix(prefix, name)
+
+    @pytest.mark.parametrize("prefix, name, key", whitespace_strings_key, ids=["whitespace test"])
+    def test_tag_data_key(self, prefix, name, key):
+        # Expected key to be generated
+        expected_key = f"{prefix}/NODE/{name}/TAGS/DATA/{key}"
+
+        assert expected_key == keys.tag_data_key(prefix, name, key)
+
+    @pytest.mark.parametrize("prefix, name", whitespace_strings_no_key, ids=["whitespace test"])
+    def test_tag_write_key_prefix(self, prefix, name):
+        # Expected key to be generated
+        expected_key = f"{prefix}/NODE/{name}/TAGS/WRITE"
+
+        assert expected_key == keys.tag_write_key_prefix(prefix, name)
+
+    @pytest.mark.parametrize("prefix, name, key", whitespace_strings_key, ids=["whitespace test"])
+    def test_tag_write_key(self, prefix, name, key):
+        # Expected key to be generated
+        expected_key = f"{prefix}/NODE/{name}/TAGS/WRITE/{key}"
+
+        assert expected_key == keys.tag_write_key(prefix, name, key)
+
+    @pytest.mark.parametrize("prefix, name", whitespace_strings_no_key, ids=["whitespace test"])
+    def test_state_key_prefix(self, prefix, name):
+        # Expected key to be generated
+        expected_key = f"{prefix}/NODE/{name}/STATE"
+
+        assert expected_key == keys.state_key_prefix(prefix, name)
+
+    @pytest.mark.parametrize("prefix, name", whitespace_strings_no_key, ids=["whitespace test"])
+    def test_method_key_prefix(self, prefix, name):
+        # Expected key to be generated
+        expected_key = f"{prefix}/NODE/{name}/METHODS"
+
+        assert expected_key == keys.method_key_prefix(prefix, name)
+
+    @pytest.mark.parametrize("prefix, name", whitespace_strings_no_key, ids=["whitespace test"])
+    def test_liveliness_key_prefix(self, prefix, name):
+        # Expected key to be generated
+        expected_key = f"{prefix}/NODE/{name}"
+
+        assert expected_key == keys.liveliness_key_prefix(prefix, name)
+
+    @pytest.mark.parametrize("prefix, name", whitespace_strings_no_key, ids=["whitespace test"])
+    def test_subnodes_key_prefix(self, prefix, name):
+        # Expeced key to be generated
+        expected_key = f"{prefix}/NODE/{name}/SUBNODES"
+
+        assert expected_key == keys.subnodes_key_prefix(prefix, name)
+
+    @pytest.mark.parametrize("prefix, name, key", whitespace_strings_key, ids=["whitespace test"])
+    def test_method_response_from_call(self, prefix, name, key):
+        # Expected key to be generated
+        expected_key = f"{prefix}/NODE{name}/METHODS/{key}/RESPONSE"
+
+        assert expected_key == keys.method_response_from_call(key_expr=f"{prefix}/NODE{name}/METHODS/{key}")
+
+    @pytest.mark.parametrize("prefix, name", whitespace_strings_no_key, ids=["whitespace test"])
+    def test_internal_to_user_key(self, prefix, name):
+        # Generated node key from node_key_prefix
+        node_key = keys.node_key_prefix(prefix, name)
+
+        # Expected key to be generated
+        expected_key = f"{prefix}/{name}"
         assert expected_key == keys.internal_to_user_key(key_expr=node_key)
 
 class TestEmpty:
     def test_key_join(self):
+        # key_join when fed no arguments should return an empty string
+
         assert '' == keys.key_join()
 
     def test_node_key_prefix(self):
+        # Expected key to be generated
         expected_key = "/NODE/"
+
         assert  expected_key == keys.node_key_prefix(prefix="", name="")
 
     def test_meta_key_prefix(self):
+        # Expected key to be generated
         expected_key = "/NODE//META"
+
         assert expected_key == keys.meta_key_prefix(prefix="", name="")
 
     def test_tag_data_key_prefix(self):
+        # Expected key to be generated
         expected_key = "/NODE//TAGS/DATA"
+        
         assert expected_key == keys.tag_data_key_prefix(prefix="", name="")
 
     def test_tag_data_key(self):
+        # Expected key to be generated
         expected_key = "/NODE//TAGS/DATA/"
+
         assert expected_key == keys.tag_data_key(prefix="", name="", key="")
 
     def test_tag_write_key_prefix(self):
+        # Expected key to be generated
         expected_key = "/NODE//TAGS/WRITE"
+
         assert expected_key == keys.tag_write_key_prefix(prefix="", name="")
 
     def test_tag_write_key(self):
+        # Expected key to be generated
         expected_key = "/NODE//TAGS/WRITE/"
+
         assert expected_key == keys.tag_write_key(prefix="", name="", key="")
 
     def test_state_key_prefix(self):
+        # Expected key to be generated
         expected_key = "/NODE//STATE"
+
         assert expected_key == keys.state_key_prefix(prefix="", name="")
 
     def test_method_key_prefix_empty_param(self):
+        # Expected key to be generated
         expected_key = "/NODE//METHODS"
+
         assert expected_key == keys.method_key_prefix(prefix="", name="")
 
     def test_liveliness_key_prefix(self):
+        # Expected key to be generated
         expected_key = "/NODE/"
+
         assert expected_key == keys.liveliness_key_prefix(prefix="", name="")
 
     def test_subnodes_key_prefix(self):
+        # Expected key to be generated
         expected_key = "/NODE//SUBNODES"
+
         assert expected_key == keys.subnodes_key_prefix(prefix="", node_name="")
 
     def test_method_response_from_call(self):
+        # Expected key to be generated
         expected_key = "/RESPONSE"
+
         assert expected_key == keys.method_response_from_call(key_expr="")
 
     def test_internal_to_user_key(self):
+        # Checks that a "ValueError" exception is thrown and that the exact message is "'NODE' is not in list"
         with pytest.raises(ValueError, match="'NODE' is not in list"):
             keys.internal_to_user_key(key_expr="")
 
@@ -183,72 +305,96 @@ class TestUnicode:
 
     @pytest.mark.parametrize("prefix, name, key", UnicodeChars, ids=ids)
     def test_key_join(self, prefix, name, key):
+        # Expected key to be generated
         expected_key = f"{prefix}/{name}/{key}"
+
         assert expected_key == keys.key_join(prefix, name, key)
 
     @pytest.mark.parametrize("prefix, name, key", UnicodeChars, ids=ids)
     def test_node_key_prefix(self, prefix, name, key):
+        # Expected key to be generated
         expected_key = f"{prefix}/NODE/{name}/{key}"
 
         assert expected_key == keys.node_key_prefix(prefix=prefix, name=f"{name}/{key}")
 
     @pytest.mark.parametrize("prefix, name, key", UnicodeChars, ids=ids)
     def test_meta_key_prefix(self, prefix, name, key):
+        # Expected key to be generated
         expected_key = f"{prefix}/NODE/{name}/{key}/META"
     
         assert expected_key == keys.meta_key_prefix(prefix=prefix, name=f"{name}/{key}")
 
     @pytest.mark.parametrize("prefix, name, key", UnicodeChars, ids=ids)
     def test_tag_data_key_prefix(self, prefix, name, key):
+        # Expected key to be generated
         expected_key = f"{prefix}/NODE/{name}/{key}/TAGS/DATA"
     
         assert expected_key == keys.tag_data_key_prefix(prefix=prefix, name=f"{name}/{key}")
 
     @pytest.mark.parametrize("prefix, name, key", UnicodeChars, ids=ids)
     def test_tag_data_key(seself, prefix, name, key):
+        # Expected key to be generated
         expected_key = f"{prefix}/NODE/{name}/TAGS/DATA/{key}"
 
         assert expected_key == keys.tag_data_key(prefix=prefix, name=name, key=key)
 
     @pytest.mark.parametrize("prefix, name, key", UnicodeChars, ids=ids)
     def test_tag_write_key_prefix(self, prefix, name, key):
+        # Expected key to be generated
         expected_key = f"{prefix}/NODE/{name}/{key}/TAGS/WRITE"
+
         assert expected_key == keys.tag_write_key_prefix(prefix=prefix, name=f"{name}/{key}")
 
     @pytest.mark.parametrize("prefix, name, key", UnicodeChars, ids=ids)
     def test_tag_write_key(self, prefix, name, key):
+        # Expected key to be generated
         expected_key = f"{prefix}/NODE/{name}/TAGS/WRITE/{key}"
+
         assert expected_key == keys.tag_write_key(prefix=prefix, name=name, key=key)
 
     @pytest.mark.parametrize("prefix, name, key", UnicodeChars, ids=ids)
     def test_state_key_prefix(self, prefix, name, key):
+        # Expected key to be generated
         expected_key = f"{prefix}/NODE/{name}/{key}/STATE"
+
         assert expected_key == keys.state_key_prefix(prefix=prefix, name=f"{name}/{key}")
 
     @pytest.mark.parametrize("prefix, name, key", UnicodeChars, ids=ids)
     def test_method_key_prefix(self, prefix, name, key):
+        # Expected key to be generated
         expected_key = f"{prefix}/NODE/{name}/{key}/METHODS"
+
         assert expected_key == keys.method_key_prefix(prefix=prefix, name=f"{name}/{key}")
 
     @pytest.mark.parametrize("prefix, name, key", UnicodeChars, ids=ids)
     def test_liveliness_key_prefix(self, prefix, name, key):
+        # Expected key to be generated
         expected_key = f"{prefix}/NODE/{name}/{key}"
+
         assert expected_key == keys.liveliness_key_prefix(prefix=prefix, name=f"{name}/{key}")
 
     @pytest.mark.parametrize("prefix, name, key", UnicodeChars, ids=ids)
     def test_subnodes_key_prefix(self, prefix, name, key):
+        # Expected key to be generated
         expected_key = f"{prefix}/NODE/{name}/{key}/SUBNODES"
+
         assert expected_key == keys.subnodes_key_prefix(prefix=prefix, node_name=f"{name}/{key}")
 
     @pytest.mark.parametrize("prefix, name, key", UnicodeChars, ids=ids)
     def test_method_response_from_call(self, prefix, name, key):
+        # Expected key to be generated
         expected_key = f"{prefix}/{name}/{key}/RESPONSE"
+
         assert expected_key == keys.method_response_from_call(key_expr=f"{prefix}/{name}/{key}")
 
     @pytest.mark.parametrize("prefix, name, key", UnicodeChars, ids=ids)
     def test_internal_to_user_key(self, prefix, name, key):
+        # Generated node key from node_key_prefix
         node_key = keys.node_key_prefix(prefix=prefix, name=name)
+        
+        # Expected key to be generated
         expected_key = f"{prefix}/{name}"
+
         assert expected_key == keys.internal_to_user_key(key_expr=node_key)
 
 class TestOverlap:
--- test/test_node/test_remote.py
+++ test/test_node/test_remote.py
@@ -1,2 +1,174 @@
-# To be honest I don't even know if we're going to be able to test remote.py without refactoring a ton of it to use MockComm
-# but then again we might be able to, I'm just a tester \(._.)/
+import pytest
+import gedge
+import pathlib
+from collections import defaultdict
+
+class TestSanity:
+
+    def test_add_tag_data_callback(self):
+    
+        def tag_handler(query: gedge.TagWriteQuery) -> None:
+            if query.value > 10:
+                query.reply(400)
+                return
+            query.reply(200)
+        
+
+        here = pathlib.Path(__file__).parent
+    
+        config = gedge.NodeConfig.from_json5(str(here / "test_writee.json5"))
+    
+        config.add_tag_write_handler("tag/write", handler=tag_handler)
+
+        node = gedge.mock_connect(config)
+
+        remote = node.connect_to_remote("test/tag/writes/writee")
+
+        comm = remote._comm
+
+        assert comm.subscribers == defaultdict(list)
+
+        def tag_data_callback_handler():
+            print("Yeah dawg")
+
+        remote.add_tag_data_callback("tag/write", tag_data_callback_handler)        
+
+        assert comm.subscribers != []
+
+    def test_add_state_callback(self):
+        
+        here = pathlib.Path(__file__).parent
+    
+        config = gedge.NodeConfig.from_json5(str(here / "test_writee.json5"))
+
+        config.tags['tag/write']._writable = False
+
+        node = gedge.mock_connect(config)
+
+        remote = node.connect_to_remote("test/tag/writes/writee")
+
+        comm = remote._comm
+
+        assert comm.subscribers == defaultdict(list)
+
+        def state_callback_handler():
+            print("Yeah dawg")
+
+        remote.add_state_callback(state_callback_handler)
+
+        assert comm.subscribers != []
+
+    def test_add_meta_callback(self):
+        
+        here = pathlib.Path(__file__).parent
+    
+        config = gedge.NodeConfig.from_json5(str(here / "test_writee.json5"))
+
+        config.tags['tag/write']._writable = False
+
+        node = gedge.mock_connect(config)
+
+        remote = node.connect_to_remote("test/tag/writes/writee")
+
+        comm = remote._comm
+
+        assert comm.subscribers == defaultdict(list)
+
+        def meta_callback_handler():
+            print("Yeah dawg")
+
+        remote.add_meta_callback(meta_callback_handler)
+
+        assert comm.subscribers != []
+
+    # MockComm doesn't have a session attribute, so liveliness callback can't be initialized
+    @pytest.mark.skip
+    def test_add_liveliness_callback(self):
+        
+        here = pathlib.Path(__file__).parent
+    
+        config = gedge.NodeConfig.from_json5(str(here / "test_writee.json5"))
+
+        config.tags['tag/write']._writable = False
+
+        node = gedge.mock_connect(config)
+
+        remote = node.connect_to_remote("test/tag/writes/writee")
+
+        comm = remote._comm
+
+        assert comm.subscribers == defaultdict(list)
+
+        def liveliness_callback_handler():
+            print("Yeah dawg")
+
+        remote.add_liveliness_callback(liveliness_callback_handler)
+
+        assert comm.subscribers != []
+
+
+    # MockComm doesn't have a session attribute, so TagBind can't be initialized
+
+    @pytest.mark.skip
+    def test_tag_binds(self):
+
+        # Create some tags
+
+        def tag_handler(query: gedge.TagWriteQuery) -> None:
+            if query.value > 10:
+                query.reply(400)
+                return
+            query.reply(200)
+        
+
+        here = pathlib.Path(__file__).parent
+    
+        config = gedge.NodeConfig.from_json5(str(here / "test_writee.json5"))
+    
+        config.add_tag_write_handler("tag/write", handler=tag_handler)
+
+        config.add_tag("tag1/write", int, {"desc": "testing a tag write",})
+
+        config.add_tag("tag2/write", int, {"desc": "testing a tag write",})
+
+        config.add_tag("tag3/write", int, {"desc": "testing a tag write",})
+
+        config.add_tag("tag4/write", int, {"desc": "testing a tag write",})
+
+        node = gedge.mock_connect(config)
+
+        remote = node.connect_to_remote("test/tag/writes/writee")
+
+        remote.tag_binds(["tag/write","tag1/write","tag2/write","tag3/write","tag4/write"])
+
+
+        pytest.fail("Yeah dawg")
+
+    # MockComm doesn't have a session attribute, so TagBind can't be initialized
+
+    @pytest.mark.skip
+    def test_tag_bind(self):
+
+        # Create some tags
+
+        def tag_handler(query: gedge.TagWriteQuery) -> None:
+            if query.value > 10:
+                query.reply(400)
+                return
+            query.reply(200)
+        
+
+        here = pathlib.Path(__file__).parent
+    
+        config = gedge.NodeConfig.from_json5(str(here / "test_writee.json5"))
+    
+        config.add_tag_write_handler("tag/write", handler=tag_handler)
+
+        node = gedge.mock_connect(config)
+
+        remote = node.connect_to_remote("test/tag/writes/writee")
+
+        remote.tag_bind("tag/write")
+
+
+        pytest.fail("Yeah dawg")
